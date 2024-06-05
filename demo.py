6485af6809a7217aa8f5526efa9836a54fc1d994
sree <prr693603@gmail.com>
2024-06-05 11:17:49 +0530

    new

---
+++ demo.py
@@ -0,0 +1,109 @@
+'''print('hello','hi')
+age=20
+name='sreeraj'
+weight=60
+
+print('name is :',name)
+print('weight is :',weight)
+print('age is :',age)
+
+a=10
+print(type(a))
+b=20.5
+print(type(b))
+c=2+5j
+print(type(c))
+name='sree'
+print(type(name))
+d=True
+print(type(d))
+e=False
+print(type(e))
+i=[10,20,30]
+print(type(i))
+f=(10,20,30)
+print(type(f))
+s={10,20,30}
+print(type(s))
+x={'name ':'sree'}
+print(type(x))'''
+
+'''age=int(input('enter age'))
+Name=str(input('enter name'))
+weight=float(input('enter weight'))
+
+print('age :',age)
+print('Name :',Name)
+print('weight :',weight)'''
+
+'''print(10+5,'welcome'+'manu') 
+print(10-5)
+print(10*5,'python'*2)
+print(10/5)
+print(10%2)
+print(10**3)
+print(10//3)'''
+
+'''print(10==10,10==11)
+print(10!=10,10!=11)
+print(10>5)
+print(10<5)
+print(10>=5,10>=20)
+print(10<=11)
+
+a=10
+a+=20
+print(a)
+
+
+a=20
+a*=10
+print(a)
+
+a=20
+a/=10
+print(a)
+
+a=20
+a**=10
+print(a)
+
+a=20
+a//=10
+print(a)
+
+a=20
+a-=10
+print(a)
+
+print(10==10 and 10<=5)
+print(10==10 and 10>=5)
+
+print(10==10 or 10>=5)
+print(10==5 or 10<=20)
+
+print(not(10==10))
+
+a='welcome'
+print('e' in a)
+print('z'in a)
+print('e'not in a)
+print('z'not in a)'''
+
+a=10
+b=10
+print(id(a))
+print(id(b))
+print(a is b)
+print(a is not b)
+
+a=[10,20]
+b=[10,20]
+print(id(a))
+print(id(b))
+print(a is b)
+print(a is not b)
+
+
+
+
